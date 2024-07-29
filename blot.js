ad8ea082a0b44bc6eeb48575f9f3f7bbf6cab0e9
blaze <Arnnav0kudale@gmail.com>
2024-07-29 06:56:22 +0530

    grid lines and stuff

--- blot.js
+++ blot.js
@@ -1,57 +1,61 @@
+const initialBoard = [
+  "rnbqkbnr",
+  "pppppppp",
+  "        ",
+  "        ",
+  "        ",
+  "        ",
+  "PPPPPPPP",
+  "RNBQKBNR",
+];
+
 const width = 200;
 const height = 200;
+const gridSize = 8;
+const squareSize = width / gridSize;
+const pieces = {
+  K: drawKing,
+  Q: drawQueen,
+  R: drawRook,
+  B: drawBishop,
+  N: drawKnight,
+  P: drawPawn,
+  k: drawKingFilled,
+  q: drawQueenFilled,
+  r: drawRookFilled,
+  b: drawBishopFilled,
+  n: drawKnightFilled,
+  p: drawPawnFilled,
+};
+const finalLines = [];
 
 setDocDimensions(width, height);
 
-// Store final lines here
-const finalLines = [];
+for (let x = 0; x <= gridSize; x++) {
+  finalLines.push([
+    [x * squareSize, 0],
+    [x * squareSize, height],
+  ]);
+}
 
-// Size of each square
-const squareSize = width / 8;
+for (let y = 0; y <= gridSize; y++) {
+  finalLines.push([
+    [0, y * squareSize],
+    [width, y * squareSize],
+  ]);
+}
 
-// Map of pieces to drawing functions
-const pieces = {
-  'K': drawKing,
-  'Q': drawQueen,
-  'R': drawRook,
-  'B': drawBishop,
-  'N': drawKnight,
-  'P': drawPawn,
-  'k': drawKingFilled,
-  'q': drawQueenFilled,
-  'r': drawRookFilled,
-  'b': drawBishopFilled,
-  'n': drawKnightFilled,
-  'p': drawPawnFilled,
-};
-
-// Initial position of pieces on a chessboard
-const initialBoard = [
-  'rnbqkbnr',
-  'pppppppp',
-  '        ',
-  '        ',
-  '        ',
-  '        ',
-  'PPPPPPPP',
-  'RNBQKBNR'
-];
-
-// Draw pieces on the board
 for (let row = 0; row < 8; row++) {
   for (let col = 0; col < 8; col++) {
     const x = col * squareSize;
     const y = row * squareSize;
     const piece = initialBoard[row][col];
-    if (piece !== ' ' && pieces[piece]) {
+    if (piece !== " " && pieces[piece]) {
       pieces[piece](x, y, squareSize);
     }
   }
 }
 
-// Drawing functions for each piece (same as before)
-
-// Draw King
 function drawKing(x, y, size) {
   const polyline = [
     [x + size * 0.5, y + size * 0.1],
@@ -63,7 +67,7 @@ function drawKing(x, y, size) {
     [x + size * 0.3, y + size * 0.7],
     [x + size * 0.7, y + size * 0.7],
     [x + size * 0.3, y + size * 0.9],
-    [x + size * 0.7, y + size * 0.9]
+    [x + size * 0.7, y + size * 0.9],
   ];
   finalLines.push(polyline);
 }
@@ -80,13 +84,12 @@ function drawKingFilled(x, y, size) {
       [x + size * 0.3, y + size * 0.7 + i * 0.01 * size],
       [x + size * 0.7, y + size * 0.7 + i * 0.01 * size],
       [x + size * 0.3, y + size * 0.9 + i * 0.01 * size],
-      [x + size * 0.7, y + size * 0.9 + i * 0.01 * size]
+      [x + size * 0.7, y + size * 0.9 + i * 0.01 * size],
     ];
     finalLines.push(polyline);
   }
 }
 
-// Draw Queen
 function drawQueen(x, y, size) {
   const polyline = [
     [x + size * 0.5, y + size * 0.1],
@@ -119,7 +122,6 @@ function drawQueenFilled(x, y, size) {
   }
 }
 
-// Draw Rook
 function drawRook(x, y, size) {
   const polyline = [
     [x + size * 0.3, y + size * 0.1],
@@ -130,7 +132,7 @@ function drawRook(x, y, size) {
     [x + size * 0.7, y + size * 0.7],
     [x + size * 0.7, y + size * 0.9],
     [x + size * 0.3, y + size * 0.9],
-    [x + size * 0.3, y + size * 0.1]
+    [x + size * 0.3, y + size * 0.1],
   ];
   finalLines.push(polyline);
 }
@@ -146,13 +148,12 @@ function drawRookFilled(x, y, size) {
       [x + size * 0.7, y + size * 0.7 + i * 0.01 * size],
       [x + size * 0.7, y + size * 0.9 + i * 0.01 * size],
       [x + size * 0.3, y + size * 0.9 + i * 0.01 * size],
-      [x + size * 0.3, y + size * 0.1 + i * 0.01 * size]
+      [x + size * 0.3, y + size * 0.1 + i * 0.01 * size],
     ];
     finalLines.push(polyline);
   }
 }
 
-// Draw Bishop
 function drawBishop(x, y, size) {
   const polyline = [
     [x + size * 0.5, y + size * 0.1],
@@ -163,7 +164,7 @@ function drawBishop(x, y, size) {
     [x + size * 0.7, y + size * 0.3],
     [x + size * 0.5, y + size * 0.5],
     [x + size * 0.3, y + size * 0.9],
-    [x + size * 0.7, y + size * 0.9]
+    [x + size * 0.7, y + size * 0.9],
   ];
   finalLines.push(polyline);
 }
@@ -179,13 +180,12 @@ function drawBishopFilled(x, y, size) {
       [x + size * 0.7, y + size * 0.3 + i * 0.01 * size],
       [x + size * 0.5, y + size * 0.5 + i * 0.01 * size],
       [x + size * 0.3, y + size * 0.9 + i * 0.01 * size],
-      [x + size * 0.7, y + size * 0.9 + i * 0.01 * size]
+      [x + size * 0.7, y + size * 0.9 + i * 0.01 * size],
     ];
     finalLines.push(polyline);
   }
 }
 
-// Draw Knight
 function drawKnight(x, y, size) {
   const polyline = [
     [x + size * 0.5, y + size * 0.1],
@@ -197,7 +197,7 @@ function drawKnight(x, y, size) {
     [x + size * 0.4, y + size * 0.6],
     [x + size * 0.6, y + size * 0.6],
     [x + size * 0.3, y + size * 0.9],
-    [x + size * 0.7, y + size * 0.9]
+    [x + size * 0.7, y + size * 0.9],
   ];
   finalLines.push(polyline);
 }
@@ -214,13 +214,12 @@ function drawKnightFilled(x, y, size) {
       [x + size * 0.4, y + size * 0.6 + i * 0.01 * size],
       [x + size * 0.6, y + size * 0.6 + i * 0.01 * size],
       [x + size * 0.3, y + size * 0.9 + i * 0.01 * size],
-      [x + size * 0.7, y + size * 0.9 + i * 0.01 * size]
+      [x + size * 0.7, y + size * 0.9 + i * 0.01 * size],
     ];
     finalLines.push(polyline);
   }
 }
 
-// Draw Pawn
 function drawPawn(x, y, size) {
   const polyline = [
     [x + size * 0.5, y + size * 0.1],
@@ -230,7 +229,7 @@ function drawPawn(x, y, size) {
     [x + size * 0.3, y + size * 0.3],
     [x + size * 0.7, y + size * 0.3],
     [x + size * 0.3, y + size * 0.9],
-    [x + size * 0.7, y + size * 0.9]
+    [x + size * 0.7, y + size * 0.9],
   ];
   finalLines.push(polyline);
 }
@@ -245,11 +244,10 @@ function drawPawnFilled(x, y, size) {
       [x + size * 0.3, y + size * 0.3 + i * 0.01 * size],
       [x + size * 0.7, y + size * 0.3 + i * 0.01 * size],
       [x + size * 0.3, y + size * 0.9 + i * 0.01 * size],
-      [x + size * 0.7, y + size * 0.9 + i * 0.01 * size]
+      [x + size * 0.7, y + size * 0.9 + i * 0.01 * size],
     ];
     finalLines.push(polyline);
   }
 }
 
-// Draw all final lines
 drawLines(finalLines);
